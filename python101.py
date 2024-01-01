2983b8986abbf472d04c691107f672057a42ba18
Abel-B3lla <bel8794@gmail.com>
2024-01-01 14:52:30 +0300

    strings, numbers and math

---
+++ python101.py
@@ -0,0 +1,22 @@
+print("Hello\nWorld")
+
+chisness = "Bella is chis"
+print(chisness.upper().isupper()) # covers t upper case and check it's converted
+print(len(chisness)) # to find the length(count of characters)
+print(chisness[0:3]) # to find a specific charachters in a string
+print(chisness.index("ll")) # to find the index of a value
+print(chisness.replace('chis','cool')) # to replace a value
+print(chisness) # it doest change the original value
+
+print(1587 % 3) # mod... tell the remainder after dividing... if remainder is 0, can be devidable without a decimal 
+print(pow(2,3)) # 2 rise to(to the power of) 3
+print(max(6.5878,6.56513)) # to find the max, we can use also min
+print(round(3.453434)) # round to the nearest whole number
+
+from math import * # to import math functions
+print(floor(3.852)) # always rounds down
+print(ceil(3.12)) # always rounds up
+print(sqrt(9)) # the square root
+# print(input("Name: ")) # to get values from user
+print(int("2")) # to convert to whole number
+print(float('2.3')) # to convert to decimal 

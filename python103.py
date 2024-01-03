8fc4a42417b376df23e3ff41b6f1f663557a016d
Abel-B3lla <bel8794@gmail.com>
2024-01-03 10:08:36 +0300

    for loops

--- python103.py
+++ python103.py
@@ -2,3 +2,18 @@ def say_hi(name, age):
   return 'Hello, ' + "i'm, " + str(name),  str(age)
 
 print(say_hi("Abel",29))
+
+
+for i in "Bella is Chis":
+  print(i)
+
+nums = [2,6,5,9]
+for i in nums:
+  print(i)
+
+for i in range(2,5):
+  print(i)
+
+friends = ["ABD", "Aman", "Ashe", "Faya"]
+for i in range(len(friends)):
+  print(friends[i])

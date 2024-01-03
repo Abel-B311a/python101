57193da7706bb74dbd9d837a7e523acc074ef94e
Abel-B3lla <bel8794@gmail.com>
2024-01-03 12:35:48 +0300

    manipulate a file

--- python104.py
+++ python104.py
@@ -7,3 +7,14 @@ except ZeroDivisionError: # good to sepcify it
   print("Div by 0")
 except: # to brord 
   print("Invalid input")
+
+
+pyfile = open("python101.py","r") # to open a file as r = read, w write, r+ read and write, a append
+#print(pyfile.readable()) # to check the file s reable 
+#print(pyfile.read()) # to actually read all info
+#print(pyfile.readline()) # to read the 1st line
+#print(pyfile.readline()) # to read the 2nd line, 
+#print(pyfile.readlines()) # to put every line in alist array
+#print(pyfile.readlines()[2]) # to access the line in the list
+for item in pyfile.readlines():
+  print(item) # we can loop over the lines

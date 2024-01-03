758a269030fb9386e47d0e68030423dacdfeee7e
Abel-B3lla <bel8794@gmail.com>
2024-01-03 12:10:03 +0300

    try except

--- python104.py
+++ python104.py
@@ -1,2 +1,9 @@
-print(2**3)
-print(2^3)
+try:
+  10/0
+  num = int(input("Insert a number: "))
+  print(num)
+
+except ZeroDivisionError: # good to sepcify it
+  print("Div by 0")
+except: # to brord 
+  print("Invalid input")

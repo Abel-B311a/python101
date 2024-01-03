01519eaeaafcbf37f1631d1cf1fc2e2ebabd4fb0
Abel-B3lla <bel8794@gmail.com>
2024-01-03 15:10:10 +0300

    read, write, append

--- python104.py
+++ python104.py
@@ -9,7 +9,7 @@ except: # to brord
   print("Invalid input")
 
 
-pyfile = open("python101.py","r") # to open a file as r = read, w write, r+ read and write, a append
+pyfile = open("python101.py","a") # to open a file as r = read, w write, r+ read and write, a append
 #print(pyfile.readable()) # to check the file s reable 
 #print(pyfile.read()) # to actually read all info
 #print(pyfile.readline()) # to read the 1st line
@@ -18,3 +18,8 @@ pyfile = open("python101.py","r") # to open a file as r = read, w write, r+ read
 #print(pyfile.readlines()[2]) # to access the line in the list
 for item in pyfile.readlines():
   print(item) # we can loop over the lines
+pyfile.close() # good practice to close it
+
+
+python101.py.write("\nBella is lela tarik")   # note: using a(append)each time we run the program it will add it to the file and \n to make it on new line
+# using "w", it will replace all the content of the file with the new value we write and if we choose to open new file name, it will create a file for it with the value added, u can use diffrent file types like .html

ca61885c319de4b745a84c7dd23d8196ff645837
Abel-B3lla <bel8794@gmail.com>
2024-01-04 22:48:00 +0300

    quantifires

--- python107.py
+++ python107.py
@@ -30,7 +30,13 @@ Quantifiers:
 [a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+
 """
 import re
-pattern = re.compile(r'\d\d\d.\d\d\d.\d\d\d\d')
+#pattern = re.compile(r'\d\d\d.\d\d\d.\d\d\d\d')# all phone nums with any character in the middle
+#pattern = re.compile(r'\d\d\d[-]\d\d\d[-]\d\d\d\d') # phone nums with only - in the middle (we dont need to escape\ the charaters one they are in [])
+#pattern = re.compile(r'\d\d\d[-.]\d\d\d[-.]\d\d\d\d') #phone nums with - OR . characters in the middle(Note onece it find one character it ignores the other, it only match one character in the set[])
+#pattern = re.compile(r'[89]00[-]\d\d\d[-]\d\d\d\d') # phone nums that start with 800 or 900
+pattern = re.compile(r'\d{3}[-]\d{3}[-]\d{4}') # instead of writing every digit, we can use quntifiers to specify howmany times a character exist
+#pattern = re.compile(r'[1-5]') # when the - is in middle of characters it tells the range in this case 1 to 5, or [a-zA-Z] (the range of all lower or uper case letters). if its in the beginning or ending it find the character - litrally
+#pattern = re.compile(r'[^a-zA-Z]') #if we use the carat^ before the set ^[], all characters that start in the charcters in the set. if it is inside the set[^ ] it negates, i.e. all character that NOT start in the character set
 with open('data.txt', 'r') as f:
   contents = f.read()
   matches = pattern.finditer(contents)

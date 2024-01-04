859264987afb7a86646b1e65f539d6704c8a175c
Abel-B3lla <bel8794@gmail.com>
2024-01-04 23:52:39 +0300

    updated

--- python107.py
+++ python107.py
@@ -29,6 +29,7 @@ Quantifiers:
 
 [a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+
 """
+
 import re
 #pattern = re.compile(r'\d\d\d.\d\d\d.\d\d\d\d')# all phone nums with any character in the middle
 #pattern = re.compile(r'\d\d\d[-]\d\d\d[-]\d\d\d\d') # phone nums with only - in the middle (we dont need to escape\ the charaters one they are in [])
@@ -37,7 +38,7 @@ import re
 #pattern = re.compile(r'\d{3}[-]\d{3}[-]\d{4}') # instead of writing every digit, we can use quntifiers to specify howmany times a character exist
 #pattern = re.compile(r'[1-5]') # when the - is in middle of characters it tells the range in this case 1 to 5, or [a-zA-Z] (the range of all lower or uper case letters). if its in the beginning or ending it find the character - litrally
 #pattern = re.compile(r'[^a-zA-Z]') #if we use the carat^ before the set ^[], all characters that start in the charcters in the set. if it is inside the set[^ ] it negates, i.e. all character that NOT start in the character set
-pattern = re.compile(r'[a-zA-Z0-9.-]+@[a-zA-Z-]+[.](com|edu|net') # it result one or more characters in small or upper case or a dot or digits or hyphen till it find @ and again one or more characters in small or upper case or hyphen till it find . and one of the group com or edu or net at the end
+pattern = re.compile(r'[a-zA-Z0-9.-]+@[a-zA-Z-]+[.](com|edu|net)') # it result one or more characters in small or upper case or a dot or digits or hyphen till it find @ and again one or more characters in small or upper case or hyphen till it find . and one of the group com or edu or net at the end
 with open('data.txt', 'r') as f:
   contents = f.read()
   matches = pattern.finditer(contents)

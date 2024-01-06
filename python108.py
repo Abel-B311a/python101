0a74e9c33eca9ca970405e80f7105421c3249a34
Abel-B3lla <bel8794@gmail.com>
2024-01-06 20:32:44 +0300

    update

--- python108.py
+++ python108.py
@@ -18,8 +18,9 @@ print(b[1,2])
 print(b[0,:]) #the 1st row
 print(b[:,1]) #the 2nd column
 # to change it just set the accessed value to a value
-b[1,1] = 55
-b[:0] = 9
+b[1,1] = 55 # to change a specific value
+b[:,1] = 9 # to change the whole column with same value
+b[1,:] = [85,22,19] # to change the whole row with specified values
 print(b)
 
 
